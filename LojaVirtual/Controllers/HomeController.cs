816bd2627abbd4e4fdc928f92f7b1f47ca963f63
weslley matos <weslleymatos.github@gmail.com>
2019-08-26 14:39:28 -0300

    Views necessárias criadas

---
+++ LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            string nome = HttpContext.Request.Form["nome"];
+            string email = HttpContext.Request.Form["email"];
+            string texto = HttpContext.Request.Form["texto"];
+
+            return new ContentResult() { Content = $"Dados recebidos com sucesso." +
+                $"\nNome: {nome}" +
+                $"\nEmail: {email}" +
+                $"\nTexto: {texto}" };
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+
+    }
+}
